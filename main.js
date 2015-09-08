a6d481d1a2c08f3ae62c4bdbf900a4a340dffa6b
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-09-09 00:03:45 +0530

    Pub/Sub jobs

---
+++ main.js
@@ -0,0 +1,69 @@
+Jobs = new Mongo.Collection("jobs");
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    console.log("Starting PhlatDownload");
+  });
+
+  Meteor.publish("jobs", function() {
+    return Jobs.find({creator: this.userId});
+  })
+}
+
+if (Meteor.isClient) {
+  Meteor.subscribe("jobs");
+
+  Template.jobs.helpers({
+    jobs: function() {
+      return Jobs.find({}, {sort: {creationTime: -1}});
+    }
+  });
+
+  Template.job.helpers({
+    trClass: function(status) {
+      switch(status) {
+        case 'pending':
+          return 'warning';
+        case 'running':
+          return 'info';
+        case 'failed':
+          return 'danger';
+        case 'finished':
+          return 'success';
+        default:
+          return 'active';
+      }
+    }
+  });
+
+  Template.jobForm.events({
+    "submit .new-job": function(event) {
+      event.preventDefault();
+      var jobInp = $("#job");
+      var nodeInp = $("#node");
+      Meteor.call("addJob", jobInp.val(), nodeInp.val());
+      jobInp.val("");
+      nodeInp.val("");
+    }
+  });
+}
+
+Meteor.methods({
+  addJob: function(job, node) {
+    if(!Meteor.userId()) {
+      throw new Meteor.Error("forbidden");
+    }
+
+    if(job == "" || node == "") {
+      throw new Meteor.Error("invalid-job");
+    }
+
+    Jobs.insert({
+      job: job,
+      node: node,
+      creator: Meteor.userId(),
+      status: "pending",
+      creationTime: new Date()
+    });
+  }
+});
