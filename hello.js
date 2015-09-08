c591b570843fec40d3c75142414b0e24dadfc453
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-09-08 23:10:43 +0530

    Add a method for adding job

--- hello.js
+++ hello.js
@@ -1,3 +1,5 @@
+Jobs = new Mongo.Collection("jobs");
+
 if (Meteor.isClient) {
 
 }
@@ -7,3 +9,23 @@ if (Meteor.isServer) {
     console.log("Starting PhlatDownload");
   });
 }
+
+Meteor.methods({
+  addJob: function(job, node) {
+    if(!Meteor.userId()) {
+      throw new Meteor.Error("forbidden");
+    }
+
+    if(job == "" || node == "") {
+      throw new Meteor.Error("invalid-job");
+    }
+
+    Jobs.insert({
+      job: job,
+      node: node,
+      creator: Meteor.user().username,
+      status: "pending",
+      creationTime: new Date()
+    });
+  }
+});
